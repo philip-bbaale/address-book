0fe718ab19ba6721c735704ccf744144a070bc60
Pip Bbaale <namugera.philip@gmail.com>
2020-02-25 10:29:32 +0300

    adding contact list js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
+function Contact(first, last){
+    this.firstName = first;
+    this.lastName = last;
+}
+
 $(document).ready(function(){
-    function Contact(first, last){
-        this.firstName = first;
-        this.lastName = last;
-    }
+    $("form#new-contact").submit(function(){
+        event.preventDefault();
+
+        var inputtedFirstName = $("input#new-first-name").val();
+        var inputtedLastName = $("input#new-last-name").val();
+        var newContact = new Contact(inputtedFirstName,inputtedLastName);
+
+        $("ul#contacts").append("<li><span class='contact'>" +newContact.firstName +"<span></li>");
+
+        $("input#new-first-name").val("");
+        $("input#new-last-name").val("");
+    });
 });
