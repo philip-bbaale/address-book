088d36fc165337b6367d862a96db79a2e444b7b7
Pip Bbaale <namugera.philip@gmail.com>
2020-02-26 10:35:03 +0300

    adding to contact addresses

--- js/scripts.js
+++ js/scripts.js
@@ -28,5 +28,35 @@ $(document).ready(function(){
         $("#show-contact h2").text(newContact.firstName);
         $(".first-name").text(newContact.firstName);
         $(".last-name").text(newContact.lastName);
+
+        $("ul#addresses").text("");
+        newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.County + "</li>");
+});
     });
+
+    $(".new-address").each(function() {
+        var inputtedStreet = $(this).find("input.new-street").val();
+        var inputtedCity = $(this).find("input.new-city").val();
+        var inputtedCounty = $(this).find("input.new-county").val();
+        var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty);
+        newContact.addresses.push(newAddress);
+      });
+
+    $("#add-address").click(function() {
+        $("#new-addresses").append('<div class="new-address">' +
+                                     '<div class="form-group">' +
+                                       '<label for="new-street">Street</label>' +
+                                       '<input type="text" class="form-control new-street">' +
+                                     '</div>' +
+                                     '<div class="form-group">' +
+                                       '<label for="new-city">City</label>' +
+                                       '<input type="text" class="form-control new-city">' +
+                                     '</div>' +
+                                     '<div class="form-group">' +
+                                       '<label for="new-county">County</label>' +
+                                       '<input type="text" class="form-control new-county">' +
+                                     '</div>' +
+                                   '</div>');
+      });
 });
